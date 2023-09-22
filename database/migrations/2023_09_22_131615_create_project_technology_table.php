a89e7f1285e7cfce0eb39bcc3382136cfff8aaee
marcocampisi <marco.campisi@outlook.it>
2023-09-22 15:18:55 +0200

    Add pivot table migration

---
+++ database/migrations/2023_09_22_131615_create_project_technology_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('project_id');
+            $table->unsignedInteger('technology_id');
+            $table->timestamps();
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects');
+
+            $table->foreign('technology_id')
+                ->references('id')
+                ->on('technologies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
