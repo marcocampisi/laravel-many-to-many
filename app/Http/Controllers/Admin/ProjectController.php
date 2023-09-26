8ea868f555118a318e0dd182bbde90c84cf35f67
marcocampisi <marco.campisi@outlook.it>
2023-09-26 14:21:35 +0200

    Fix delete

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -125,6 +125,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        $project->technologies()->detach();
+        
         $project->delete();
 
         return redirect()->route('projects.index');
